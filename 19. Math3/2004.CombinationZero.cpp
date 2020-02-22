b9401d974058516bd12a16ddd3752887fe762f9e
gudwnsgur <gudwnsgur12@naver.com>
2020-02-22 13:10:31 +0900

    modify

---
+++ 19. Math3/2004.CombinationZero.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int n, m;
+int cnt5, cnt2;
+ 
+int main()
+{
+    cin >> n >> m;
+ 
+    for (long long i = 5; i <= n; i *= 5)
+        cnt5 += n / i;
+    for (long long i = 5; i <= n - m; i *= 5)
+        cnt5 -= (n - m) / i;
+    for (long long i = 5; i <= m; i *= 5)
+        cnt5 -= m / i;
+    for (long long i = 2; i <= n; i *= 2)
+        cnt2 += n / i;
+    for (long long i = 2; i <= n - m; i *= 2)
+        cnt2 -= (n - m) / i;
+    for (long long i = 2; i <= m; i *= 2)
+        cnt2 -= m / i;
+ 
+    if (cnt5 >= cnt2)
+        cout << cnt2;
+    else
+        cout << cnt5;
+ 
+    return 0;
+}
