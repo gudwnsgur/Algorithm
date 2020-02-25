f8a23eb22c426f379ef4770e4bf6d05ff0c2e136
gudwnsgur <gudwnsgur12@naver.com>
2020-02-25 16:24:23 +0900

    modify 10.math2 4948.Bertrand's Postulate for find prime number

--- 10. Math2/4948.Bertrand'sPostulate.cpp
+++ 10. Math2/4948.Bertrand'sPostulate.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 
-bool prime[123456*2+1] = {false, };
+bool primeNumber[123456 * 2 + 1]; //  [0] ~ [2n+1] 
 
-int main () {
+int main() {
+	int N;
+	for (int i = 2; i <= 123456 * 2; i++) {
+		primeNumber[i] = true;
+	}
+	for (int i = 2; i * i <= 123456 * 2; i++) {
+		if (primeNumber[i]) {
+			for (int j = i * i; j <= 123456 * 2; j += i)
+				primeNumber[j] = false;
+		}
+	}
+	while (cin >> N) {
+		if (N == 0) break;
+		int count = 0;
 
-    for(int i=2 ; i*i  <= 123456*2 ; i++) {
-        if(prime[i] == false)
-            for(int j = i*i; j<=123456*2 ; j+=i) 
-                prime[j] =true;
-    }
-    int n;
-    while(cin >> n) {
-        if(n==0 )break;
-        int count = 0;
-        for(int i=n+1 ; i<=2*n ; i++) {
-            if(prime[i] == false) count ++;
-        }
-        cout << count << endl;
-    }
-    return 0;
+		for (int i = N + 1; i <= 2 * N; i++) {
+			if (primeNumber[i]) count++;
+		}
+		cout << count << endl;
+	}
+	return 0;
 }
