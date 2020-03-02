295685031ce31a1ccf9957a3b233fc7d0a87a296
gudwnsgur <gudwnsgur12@naver.com>
2020-03-02 15:18:21 +0900

    solve Programmers Lv1. Sum beside Two Numbers

---
+++ Programmers/Level1/SumOfTwoNum.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer;
+    if(a==b) return a;
+
+    int cnt = (a>b?a:b)-(a<b?a:b)+1;
+
+    if(cnt%2 != 0 ) answer = (long long)((a+b)/2)*cnt;
+    else   answer = (long long)(a+b)*(cnt/2);
+
+    return answer;
+}
