beff75047fb44a50ccf840959d135feb2144a86a
gudwnsgur <gudwnsgur12@naver.com>
2020-03-03 19:07:44 +0900

    modify Programmers Lv1.Secret Map

--- Programmers/Level1/SecretMap.cpp
+++ Programmers/Level1/SecretMap.cpp
@@ -12,29 +12,31 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 	bool* binary2 = new bool[n];
 	bool* newBi = new bool[n];
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		int first = arr1[i];
 		int second = arr2[i];
 
 		for (int j = 0; j < n; j++) {
-			if (first >= pow(n - j - 1, 2)) {
+			if (first >= pow(2, n - j - 1)) {
 				binary1[j] = true;
-				first -= pow(n - j - 1, 2);
+				first -= pow(2, n - j - 1);
 			}
-			else { binary1[j] = false; }
-			if (second >= pow(n - j - 1, 2)) {
-				binary2[j] = true;
-				second -= pow(n - j - 1, 2);
-			}
-			else { binary2[j] = false; }
+			else  binary1[j] = false; 
 
+			if (second >= pow(2, n - j - 1)) {
+				binary2[j] = true;
+				second -= pow(2, n - j - 1);
+			}
+			else binary2[j] = false; 
 		}
+
 		for (int j = 0; j < n; j++) {
 			if (binary1[j] || binary2[j]) newBi[j] = true;
 			else newBi[j] = false;
 		}
 		string line;
-		for (int j = 0; j < n; j++) { 
+		for (int j = 0; j < n; j++) {
 			if (newBi[j]) line.push_back('#');
 			else line.push_back(' ');
 		}
@@ -59,7 +61,7 @@ int main() {
 	v2.push_back(3);
 	v2.push_back(21);
 
-	vector<string> v = solution(n,v1,v2);
+	vector<string> v = solution(n, v1, v2);
 	for (string line : v) {
 		cout << line << endl;
 	}
