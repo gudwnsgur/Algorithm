f4c9a75b93346462a87f60ae114fe060d97e7efb
gudwnsgur <gudwnsgur12@naver.com>
2020-03-05 19:06:36 +0900

    자릿수 더하기

---
+++ Programmers/Level1/자릿수더하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    do {
+        answer += n%10;
+        n /= 10;
+    }while(n>0);
+
+    return answer;
+}
