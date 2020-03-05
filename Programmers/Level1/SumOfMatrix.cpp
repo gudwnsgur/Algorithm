5989c26bdc201367a23627559495e778f8ed3656
gudwnsgur <gudwnsgur12@naver.com>
2020-03-05 20:19:10 +0900

    solve Programmers Lv1. Sum Of Matrix

---
+++ Programmers/Level1/SumOfMatrix.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+	vector<vector<int>> answer;
+	for (int i = 0; i < arr1.size(); i++) {
+		vector<int> tmp;
+		for (int j = 0; j < arr1[0].size(); j++) {
+			tmp.push_back(arr1[i][j] + arr2[i][j]);
+		}
+		answer.push_back(tmp);
+	}
+	return answer;
+}
