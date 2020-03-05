4d7760172866491b41db2f2bb91d62806536d6c7
gudwnsgur <gudwnsgur12@naver.com>
2020-03-05 19:31:16 +0900

    .

--- Programmers/Level1/RemoveSmallest.cpp
+++ Programmers/Level1/RemoveSmallest.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
@@ -19,4 +19,21 @@ vector<int> solution(vector<int> arr) {
 		if (arr[i] != smallest) answer.push_back(arr[i]);
 	}
 	return answer;
+}
+
+
+vector<int> solution2(vector<int> arr) {
+	vector<int> answer;
+
+	int min = *min_element(arr.begin(), arr.end());
+	int max = *max_element(arr.begin(), arr.end());
+	// max, min 찾기
+
+	int pos = find(arr.begin(), arr.end(), min)-arr.begin() ;
+	// 원하는 원소의 index 찾기
+
+	arr.erase(arr.begin() + pos);
+	// 원하는 index 원소 지우기
+
+	return arr.empty() ? vector<int>(1,-1) : arr ;
 }
