0f20d97b0fd0ce5127bb540a76e36dabe0f38d90
gudwnsgur <gudwnsgur12@naver.com>
2020-03-10 16:08:07 +0900

    solve Programmers country124

---
+++ Programmers/Level2/Country124.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+
+	while (n > 0)
+	{
+		if (n % 3 == 0) {
+			answer = "4" + answer;
+			n = n / 3 - 1;
+		}
+		else {
+			answer = to_string(n%3) +answer;
+            n/=3;
+		}
+	}
+
+	return answer;
+}
