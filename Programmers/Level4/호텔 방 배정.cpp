0413a4d60d1c79afa858191e35e9fd923900b8e5
gudwnsgur <gudwnsgur12@naver.com>
2020-04-04 20:38:05 +0900

    [MOD] Programmers 'Lv4.호텔 방 배정' In Progress

--- Programmers/Level4/호텔 방 배정.cpp
+++ Programmers/Level4/호텔 방 배정.cpp
@@ -22,38 +22,52 @@ vector<long long> solution(long long k, vector<long long> room_number) {
 }
 */
 
-/*
-2. 효율성 시간초과
+/*  2. 효율성 시간초과
 
 vector<long long> solution(long long k, vector<long long> room_number) {
 	vector<long long> answer;
-
 	map<long long, long long> m;
-	for (int i = 0; i < room_number.size(); i++)
-	{
-		long long K = room_number[i];
-		while (m.count(K)) {
-			K++;
-		}
-		m.insert(make_pair(K, room_number[i]));
-		answer.push_back(K);
-	}
+	for (int i = 0; i < room_number.size(); i++) {
+		long long node = room_number[i];
 
+		if (m[node] != 0) {
+			while (m[node] != 0) {
+				node = m[node] + 1;
+			}
+		} // map에 node값이 존재하면 해당 노드의 value+1 값 확인
+
+		m[node] = node;
+		answer.push_back(node);
+	}
 	return answer;
 }
+
 */
 
 
+// 시간초과
 vector<long long> solution(long long k, vector<long long> room_number) {
 	vector<long long> answer;
+	map<long long, long long> m;
+	for (int i = 0; i < room_number.size(); i++) {
+		long long node = room_number[i];
 
+		if (m[node] != 0) {
+			while (m[node] != 0) {
+				node = m[node] + 1;
+			}
+		} // map에 node값이 존재하면 해당 노드의 value+1 값 확인
 
+		m[node] = node;
+		m[room_number[i]] = node;
+		answer.push_back(node);
+	}
 	return answer;
 }
 
 // testCase;
 int main() {
-	vector<long long> room = { 1,3,4,1,3,1 };
+	vector<long long> room = { 1,3,4,1,3,1,1,3,100,2,6,1,2,3,6,1,1,1,1 };
 	vector<long long> result = solution(10, room);
 	for (int i = 0; i < result.size(); i++) {
 		cout << result[i] << ' ';
