19c1b89f95156ac3cb66e0c7620b36fd075a03c4
gudwnsgur <gudwnsgur12@naver.com>
2020-08-20 16:30:15 +0900

    [ADD] BOJ '1735_분수합'

---
+++ BOJ/1735_분수합.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int GDC(int a, int b) {
+	while (b != 0) {
+		int r = a%b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+int main() {
+	int A1, B1, A2, B2;
+	cin >> A1 >> B1 >> A2 >> B2;
+	int denominator = B1*B2, numerator = A1*B2 + A2*B1;
+	
+	cout << numerator / GDC(numerator,denominator) << ' ' << denominator / GDC(numerator, denominator) << endl;
+	return 0;
+}
