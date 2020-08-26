8cb49d07ba6006a9b4b78eb83e76de546e0b7373
gudwnsgur <gudwnsgur12@naver.com>
2020-08-26 15:20:30 +0900

    [MOD] BOJ '2178'

--- BOJ/Level/28. DFS&BFS/2178.cpp
+++ BOJ/Level/28. DFS&BFS/2178.cpp
@@ -1,72 +1,56 @@
 #include <iostream>
 #include <queue>
 #include <string>
-#define MAX 101
-
 using namespace std;
 
-bool map[MAX][MAX]{ false, };
-bool visited[MAX][MAX]{ false, };
-queue<pair<int, int>> curQ; // 현재 좌표
-queue<pair<int, int>> nextQ; // 다음으로 가야 할 좌표
-int result = 1;
+bool map[100][100];
+bool visited[100][100] = { false, };
+queue<pair<int, int>> q;
 
-void bfs(int n, int m) {
-	while (!curQ.empty()) 
-	{
-		while (!curQ.empty()) 
-		{
-			if (curQ.front().first == n && curQ.front().second == m) return;
-			// 현재 좌표가 도착지점이면 return
-			nextQ.push(curQ.front());
-			curQ.pop();
-		} //현재 모든 좌표를 nextQ로 이동
+int bfs(int n, int m) {
+	q.push(make_pair(0, 0));
+	visited[0][0] = true;
 
-		while (!nextQ.empty()) {
-			int i = nextQ.front().first; // 확인할 x 좌표 : i
-			int j = nextQ.front().second; // 확인할 y 좌표 : j
+	int cnt = 0;
+	while (!q.empty()) {
+		int size = q.size();
+		cnt++;
+		for (int i = 0; i < size; i++) {
+			int first = q.front().first, second = q.front().second;
+			if (first == n-1 && second == m-1) return cnt;
 
-			// nextQ에 담긴 모든 현재 좌표의 다음 좌표를 curQ로 이동
-
-			if (map[i+1][j] && !visited[i+1][j] && i != n) {
-				curQ.push(make_pair(i + 1, j));
-				visited[i + 1][j] = true;
-			} // 아래로 이동 
-			if (map[i][j+1] && !visited[i][j+1] && j != m) {
-				curQ.push(make_pair(i, j+1));
-				visited[i][j+1] = true;
-			} // 우로 이동
-			if (i != 0 && map[i-1][j] && !visited[i-1][j] ) {
-				curQ.push(make_pair(i - 1, j));
-				visited[i - 1][j] = true;
-			} // 위로 이동
-			if (j != 0 && map[i][j-1] && !visited[i][j-1]) {
-				curQ.push(make_pair(i, j-1));
-				visited[i][j-1] = true;
-			} // 좌로 이동
-
-			nextQ.pop();
+			if (first != 0) if (map[first - 1][second] && !visited[first-1][second]) {
+				visited[first - 1][second] = true;
+				q.push(make_pair(first - 1, second));
+			}
+			if (first != n - 1) if (map[first + 1][second] && !visited[first+1][second]) {
+				visited[first + 1][second] = true;
+				q.push(make_pair(first + 1, second));
+			}
+			if (second != 0) if (map[first][second - 1] && !visited[first][second-1]) {
+				visited[first][second-1] = true;
+				q.push(make_pair(first, second - 1));
+			}
+			if (second != m - 1) if (map[first][second + 1] && !visited[first][second+1]) {
+				visited[first][second+1] = true;
+				q.push(make_pair(first, second + 1));
+			}
+			q.pop();
 		}
-		result++;
-		// 한 칸을 이동했다는 의미
 	}
 }
 
-
 int main() {
 	int n, m;
+	string str;
 	cin >> n >> m;
-	string line;
 	for (int i = 0; i < n; i++) {
-		cin >> line;
+		cin >> str;
 		for (int j = 0; j < m; j++) {
-			if (line[j] == '1') map[i][j] = true;
+			map[i][j] = str[j] == '1' ? true : false;
 		}
-	} // map 초기화
-    
-	curQ.push(make_pair(0, 0));
-	visited[0][0] = true;
-	bfs(n-1, m-1);
-	cout << result;
+	}
+
+	cout << bfs(n, m) << endl;
 	return 0;
 }
