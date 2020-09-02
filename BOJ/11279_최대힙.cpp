3f25093bd57af7efc909a4d6e8e0dd2343af7ea1
gudwnsgur <gudwnsgur12@naver.com>
2020-09-02 17:34:11 +0900

    [ADD] BOJ '11279 최대 힙' with priority_queue

---
+++ BOJ/11279_최대힙.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	priority_queue<int> pq;
+	int n, op;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> op;
+		if (!op) {
+			if (pq.empty()) cout << "0" << endl;
+			else {
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else pq.push(op);
+	}
+	return 0;
+}
