0a09774e78e11469f3e73668e6fe84b17128a17e
gudwnsgur <gudwnsgur12@naver.com>
2020-09-06 23:55:12 +0900

    [ADD] Programmers Lv1. 약수의 합

---
+++ Programmers/Level1/약수의합.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+	int answer = 1;
+	for (int i = 2; i <= n; i++) {
+		if (n%i == 0) answer += i;
+	}
+	return answer;
+}
