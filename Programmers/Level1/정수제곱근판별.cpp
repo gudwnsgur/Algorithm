5039672bb7f07b1fc3d03921b184568ba8bc1368
gudwnsgur <gudwnsgur12@naver.com>
2020-09-07 00:26:10 +0900

    [ADD] Programmers Lv1. 정수 제곱근 판별

---
+++ Programmers/Level1/정수제곱근판별.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+long long solution(long long n) {
+	long long answer = sqrt(n);
+
+	if (answer == sqrt(n)) return pow(sqrt(n) + 1, 2);
+	else return -1;
+}
