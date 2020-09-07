b50edb3c51799dd1d44f412bfe65b38a7a1ad729
gudwnsgur <gudwnsgur12@naver.com>
2020-09-07 09:30:34 +0900

    [ADD] Programmers Lv1. 하샤드 수

---
+++ Programmers/Level1/하샤드수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(int x) {
+    int n=0, tmp=x;
+    
+    do {
+        n += tmp%10;
+        tmp /= 10;
+    }while(tmp>0);
+    
+    return !(x%n);
+}
