48197a133584366f7169ee1ef6d32edcee65a41b
gudwnsgur <gudwnsgur12@naver.com>
2020-09-08 18:54:58 +0900

    [MOD] Programmers Lv2. 조이스틱

--- Programmers/Level2/조이스틱.cpp
+++ Programmers/Level2/조이스틱.cpp
@@ -13,22 +13,19 @@ int solution(string name) {
 	}
 	if (v.size() == 0) return answer;
 
+
 	answer = v[v.size() - 1];
 	answer = min(answer, (int)name.size() - v[0]);
-	int leftMax = 0, rightMax = 0;
 
 	if (v.size() > 1) {
+		int leftMax = 0, rightMax = 0;
 		for (int i = 0; i < v.size() - 1; i++) {
 			leftMax = v[i]; 
 			rightMax = v[i + 1];
-			if (leftMax <= name.size() / 2 && rightMax >= name.size() / 2) break;
+			answer = min(answer, 2 * leftMax + ((int)name.size() - rightMax));
+			answer = min(answer, 2 * ((int)name.size() - rightMax) + leftMax);
 		}
-		answer = min(answer, 2 * leftMax + ((int)name.size() - rightMax));
-		answer = min(answer, 2 * ((int)name.size() - rightMax) + leftMax);
 	}
-
-	cout << leftMax << ' ' << rightMax << endl;
-	cout << v[v.size() - 1] << ' ' << (int)name.size() - v[0] << ' ' << 2 * leftMax + ((int)name.size() - rightMax) << ' ' << 2 * ((int)name.size() - rightMax) + leftMax << endl;
 
 	for (int i = 0; i < name.size(); i++) {
 		if (name[i] <= 'N') answer += name[i] - 65;
@@ -37,6 +34,6 @@ int solution(string name) {
 	return answer;
 }
 int main() {
-	cout << solution("ABABAAAAABA") << endl;
+	cout << solution("AAABAAAAB") << endl;
 	return 0;
 }
