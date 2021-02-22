8515bf5d95cc0cb942bf182251bf9b95f5424feb
gudwnsgur <gudwnsgur12@naver.com>
2021-02-22 17:46:21 +0900

    [ADD] BOJ 1987 알파벳 - dfs

--- BOJ/1987_알파벳.cpp
+++ BOJ/1987_알파벳.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
 #include <algorithm>
 using namespace std;
@@ -11,18 +12,7 @@ int r, c;
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
 
-void dfs(int length, int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		int nx = dx[i] + x, ny = dy[i] + y;
-		if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-		if (!visited[map[nx][ny] - 'A']) {
-			visited[map[nx][ny] - 'A'] = true;
-			result = max(length + 1, result);
-			dfs(length + 1, nx, ny);
-			visited[map[nx][ny] - 'A'] = false;
-		}
-	}
-}
+
 
 int main () {
 	cin >> r >> c;
@@ -38,7 +28,5 @@ int main () {
 	}
 	visited[map[0][0] - 'A'] = true;
 
-	dfs(1, 0, 0);
-	cout << result << endl;
 	return 0;
 }
