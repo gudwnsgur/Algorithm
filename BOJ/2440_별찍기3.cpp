acfb64d4413d16020725d89a5254928791eec989
gudwnsgur <gudwnsgur12@naver.com>
2021-03-06 15:52:08 +0900

    [ADD] BOJ 2440

---
+++ BOJ/2440_별찍기3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = n; i >= 1; i--) {
+		for (int j = 0; j < i; j++) {
+			cout << '*';
+		}cout << endl;
+	}
+
+	return 0;
+}
