0d5ec19e435f78ea36ee624374a1dea592dc060a
gudwnsgur <gudwnsgur12@naver.com>
2021-03-06 14:57:13 +0900

    [ADD] BOJ 14677 - dfs

--- BOJ/14677_병약한윤호.cpp
+++ BOJ/14677_병약한윤호.cpp
@@ -1,3 +1,5 @@
+// BFS
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -56,3 +58,53 @@ int main() {
 	return 0;
 }
 
+
+// DFS ( 재귀)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+int n, res = 0;
+char order[3] = { 'B', 'L', 'D' };
+string drug;
+vector<vector<bool>> visited;
+
+void recursive(int left, int right, int cnt, int next) {
+	res = max(cnt, res);
+
+	if (left >= drug.size() || right < 0 || left > right) {
+		res = max(cnt, res);
+		return;
+	}
+
+	if (drug[left] == order[next] && !visited[left + 1][right]) {
+		visited[left + 1][right] = true;
+		recursive(left + 1, right, cnt + 1, (next + 1) % 3);
+	}
+	if (drug[right] == order[next] && !visited[left][right-1]  ) {
+		visited[left][right-1] = true;
+		recursive(left, right - 1, cnt + 1, (next + 1) % 3);
+	}
+}
+
+int main() {
+	cin >> n;
+	cin >> drug;
+	visited = vector<vector<bool>>(drug.size() + 1, vector<bool>(drug.size() + 1, false));
+
+	if (drug[0] == 'B') {
+		visited[1][drug.size() - 1] = true;
+		recursive(1, drug.size() - 1, 1, 1);
+	}
+
+	if (drug[drug.size() - 1] == 'B') {
+		visited[0][drug.size() - 2] = true;
+		recursive(0, drug.size() - 2, 1, 1);
+
+	}
+	cout << res << endl;
+	return 0;
+}
