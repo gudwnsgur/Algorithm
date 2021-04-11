9f80f67fba765b46ed95b07072375ea2b54d06e8
gudwnsgur <gudwnsgur12@naver.com>
2021-04-11 15:11:51 +0900

    [ADD] Programmers Lv2

---
+++ Java/Programmers/Lv2_점프와순간이동.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while (n > 0) {
+            ans += n % 2;
+            n /= 2;
+        }
+        return ans;
+    }
+}
+
