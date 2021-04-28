4507cf6d316b93c2719576dfde1aa2dd4b6a68df
gudwnsgur <gudwnsgur12@naver.com>
2021-04-28 16:29:47 +0900

    [ADD] BOJ - 구현

---
+++ BOJ/14890_경사로.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tk;
+    static int n, l, result = 0;
+    static int[][] map1, map2;
+    static boolean[][] used;
+
+    static boolean isRightRoad(int loc, boolean dir, int[] road) {
+        for (int i = 0; i < road.length; i++) {
+            if (i == 0) continue;
+            if (road[i] == road[i - 1]) continue;
+            if (Math.abs(road[i] - road[i - 1]) >= 2) return false;
+
+
+            if (road[i - 1] > road[i]) {
+                int j = i, x = dir ? loc : i, y = dir ? i : loc;
+                int cnt = 0, hegiht = road[i];
+                boolean pos = false;
+
+                for (; j < road.length; j++) {
+                    if (hegiht != road[j] || used[x][y]) break;
+                    used[x][y] = true;
+                    cnt++;
+                    if (cnt == l) {
+                        pos = true;
+                        break;
+                    }
+
+                    if (dir) y++;
+                    else x++;
+                }
+                if (!pos) return false;
+                i = j;
+            }
+
+            else {
+                int x = dir ? loc : i-1, y = dir ? i-1 : loc;
+                int cnt = 0, height = road[i - 1];
+                boolean pos = false;
+
+                for (int j = i - 1; j >= 0; j--) {
+                    if (height != road[j] || used[x][y]) break;
+                    used[x][y] = true;
+                    cnt++;
+
+                    if (cnt == l) {
+                        pos = true;
+                        break;
+                    }
+                    if (dir) y--;
+                    else x--;
+                }
+                if (!pos) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        tk = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(tk.nextToken());
+        l = Integer.parseInt(tk.nextToken());
+        map1 = new int[n][n];
+        map2 = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            tk = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int value = Integer.parseInt(tk.nextToken());
+                map1[i][j] = map2[j][i] = value;
+            }
+        }
+
+        used = new boolean[n][n];
+        for (int i = 0; i < n; i++) if (isRightRoad(i, true, map1[i])) result++;
+        used = new boolean[n][n];
+        for (int i = 0; i < n; i++) if (isRightRoad(i, false, map2[i])) result++;
+
+        System.out.println(result);
+    }
+}
+
