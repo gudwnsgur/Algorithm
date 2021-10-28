9d9a5128c247086f1bd540b2360e9b90877805f0
형준혁 <gudwnsgur12@naver.com>
2021-10-28 23:47:42 +0900

    [ADD] BOJ 1735

--- BOJ/1735_분수합.cpp
+++ BOJ/1735_분수합.cpp
@@ -14,7 +14,7 @@ int main() {
 	int A1, B1, A2, B2;
 	cin >> A1 >> B1 >> A2 >> B2;
 	int denominator = B1*B2, numerator = A1*B2 + A2*B1;
-	
+	int a1, b1, a2, b2;
 	cout << numerator / GDC(numerator,denominator) << ' ' << denominator / GDC(numerator, denominator) << endl;
 	return 0;
 }
