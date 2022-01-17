85953ba97f037304cf419eda2e20f9346b11e11d
형준혁 <gudwnsgur@github.com>
2022-01-17 15:15:27 +0900

    [ADD] 1000 - Kotlin

---
+++ Kotlin/A+B.kt
@@ -0,0 +1,8 @@
+import java.util.*
+import java.io.*
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val tk = StringTokenizer(br.readLine())
+    println(Integer.parseInt(tk.nextToken()) + Integer.parseInt(tk.nextToken()))
+}
