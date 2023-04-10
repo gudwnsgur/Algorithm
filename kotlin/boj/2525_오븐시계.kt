4aead1dc7ef8313e938f294838a582c23da1f9e2
joonhyuck-hyoung <gudwnsgur12@naver.com>
2023-04-10 20:24:43 +0900

    '

---
+++ kotlin/boj/2525_오븐시계.kt
@@ -0,0 +1,11 @@
+import java.io.*
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val token = StringTokenizer(readLine())
+    val (A, B) = Integer.parseInt(token.nextToken()) to Integer.parseInt(token.nextToken())
+    val C = Integer.parseInt(readLine())
+
+    println("${(A + (B + C) / 60) % 24} ${(B + C) % 60}")
+}
+
