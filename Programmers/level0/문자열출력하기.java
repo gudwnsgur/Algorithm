b7fb90ad2937f7312d55e42f25995a650ea8d8f6
gudwnsgur <gudwnsgur12@naver.com>
2023-10-14 14:10:48 +0900

    문자열

---
+++ Programmers/level0/문자열출력하기.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println(br.readLine());
+    }
+}
